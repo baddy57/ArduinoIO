0cb14e6fa593744d8c19c459d6ac271e6bbd4e5a
Cristian Baldi <baddy227@gmail.com>
2022-11-28 18:26:12 +0100

    fix 4051port selector

--- src/Multiplexers/Mux4051.cpp
+++ src/Multiplexers/Mux4051.cpp
@@ -9,10 +9,15 @@ Mux4051::Mux4051(AddressBase* address, pin pinA, pin pinB, pin pinC) : Multiplex
 	_pinC = pinC;
 }
 
+//todo struct port to avoid runtime resolving
+
 void Mux4051::Prepare(byte port) const
 {
 	MultiplexerBase::Prepare(port);
-	digitalWrite(_pinA, port % 8);
-	digitalWrite(_pinB, port % 4);
+	digitalWrite(_pinA, port % 2);
+	port /= 2;
+	digitalWrite(_pinB, port % 2);
+	port /= 2;
 	digitalWrite(_pinC, port % 2);
+	delay(1); //todo reduce delay
 }
